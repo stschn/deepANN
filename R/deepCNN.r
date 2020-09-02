cc25f03eb3feb9080f70861ed865b67b0bae8b22
stschn <66440117+stschn@users.noreply.github.com>
2020-09-02 06:50:08 +0200

    Update

--- R/deepCNN.r
+++ R/deepCNN.r
@@ -3,31 +3,29 @@
 #' @family Convolutional Neural Network (CNN)
 #'
 #' @param images Image data either a 3D array or a list of file names, e.g. returned by \code{list.files}.
-#' @param width The width of an image, equal to the number of columns.
 #' @param height The height of an image, equal to the number of rows.
+#' @param width The width of an image, equal to the number of columns.
 #' @param channels The number of channels of an image. A color channel is a primary color (like red, green and blue), 
 #'   equal to a color valence (denotes how light effects the color sensation of an eye or in common of the brain).
 #'   Primary colors can be mixed to produce any color. 
 #'   A channel equal \code{1} indicates a grayscale image, \code{3} a color image.
 #'
-#' @return A 4D feature array with the dimensions samples (number of images), height, width and channel.
+#' @return A 4D feature array with the dimensions samples (number of images), height, width and channels.
 #' @export
 #' 
 #' @seealso \code{\link[base]{list.files}}, \code{\link[keras]{image_load}}, \code{\link[keras]{image_to_array}}, \code{\link[reticulate]{array_reshape}},
 #'   \code{\link{as.CNN.image.Y}}.
 #'
 #' @examples
-as.CNN.image.X <- function(images, width, height, channels = 3L) {
+as.CNN.image.X <- function(images, height, width, channels = 3L) {
   if (is.null(dim(images))) {
-    if (T %in% (files <- file.exists(images))) {
-      if (F %in% files) { stop("not all image files do exist.") }
-      img_list <- lapply(images, function(imgname) {
-        keras::image_load(imgname, grayscale = ifelse(channels == 1L, T, F), target_size = c(height, width))
-      })
-      img_array <- lapply(img_list, function(img) {
-        keras::image_to_array(img) # The image is in format height x width x channels
-      })
-    }
+    if (!all(file.exists(images))) { stop("images contains invalid file names.") }
+    img_list <- lapply(images, function(imgname) {
+      keras::image_load(imgname, grayscale = ifelse(channels == 1L, T, F), target_size = c(height, width))
+    })
+    img_array <- lapply(img_list, function(img) {
+      keras::image_to_array(img) # The image is in format height x width x channels
+    })
   } else {
     img_array <- images
   }
@@ -36,12 +34,8 @@ as.CNN.image.X <- function(images, width, height, channels = 3L) {
   # for (i in 1:NROW(img_array)) { feature_array[i, , , ] <- img_array[[i]] }
   
   # Option 2
-  # feature_array <- lapply(img_array, function(img) {
-  #   dim(img) <- c(1L, height, width, channels)
-  #   img
-  # })
-  # feature_array <- do.call(rbind, feature_array)
-  # dim(feature_array) <- c(NROW(feature_array), height, width, channels)
+  # feature_array <- do.call(rbind, img_array)
+  # dim(feature_array) <- c(NROW(img_array), height, width, channels)
   
   feature_array <- keras::array_reshape(img_array, c(NROW(img_array), height, width, channels))
   return(feature_array)
