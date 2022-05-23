0c3400d7bfd5fbf05d11f2e2a965a0b33f646e24
stschn <66440117+stschn@users.noreply.github.com>
2022-05-23 23:31:37 +0200

    Upgrade array

--- R/deepArray.r
+++ R/deepArray.r
@@ -675,3 +675,40 @@ flip <- function(a, axis = -1L) {
   l[axis] <- lapply(l[axis], rev)
   do.call('[', c(list(a), l))
 }
+
+#' @title Rotate an array by 90 degrees in the plane specified by axes
+#'
+#' @family Array
+#'
+#' @param a An array.
+#' @param k Number of times the array is rotated by 90 degree. Positive numbers represent clockwise rotation, negative numbers counterclockwise rotation.
+#' @param axes The array is rotated in the plane defined by the axes. Axes must be different.
+#'
+#' @details This function corresponds to \code{rot90()} from NumPy.
+#' @return A rotated view of \code{a}.
+#'
+#' @export
+rot90 <- function(a, k = 1L, axes = c(1L, 2L)) {
+  stopifnot("a must be at least a 2-dimensional array." = ndim(a) >= 2L,
+            "axes must consist of two values to spawn the plane the array is rotated." = length(axes) == 2L)
+  d <- dim(a)
+  nd <- length(d)
+  axes[which((axes < 0L) | (axes > nd))] <- nd
+  # shape of the output: d[axes] <- d[rev(axes)]
+  perm <- seq_len(deepANN::ndim(a))
+  perm[axes] <- perm[rev(axes)]
+
+  # clockwise rotation
+  if (k > 0L) {
+    for (i in seq_len(k)) {
+      a <- deepANN::transpose(deepANN::flip(a, min(axes)), perm = perm)
+    }
+  }
+  # counterclockwise rotation
+  else {
+    for (i in seq_len(abs(k))) {
+      a <- deepANN::flip(deepANN::transpose(a, perm = perm), min(axes))
+    }
+  }
+  a
+}
--- README.md
+++ README.md
@@ -28,6 +28,7 @@ This R library is currently structured in form of the following functional famil
   <li><code>insert()</code> inserts an object into an array.</li>
   <li><code>transpose()</code> transposes an array by swapping dimensions.</li>
   <li><code>flip()</code> reverses the order of the elements of an array along axes.</li>
+  <li><code>rot90()</code> rotates an array by 90 degrees in the plane specified by axes.</li>
 </ul>
 
 <b>Dummy</b>
