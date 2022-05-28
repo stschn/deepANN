857e3687894865e3fc0d5388238d0a1a1a906d65
stschn <66440117+stschn@users.noreply.github.com>
2022-05-28 18:48:22 +0200

    Update documentation

--- R/deepArray.r
+++ R/deepArray.r
@@ -796,6 +796,12 @@ rearrange <- function(a, axis = NULL) {
 #' @param axis Axis or axes along which to flip over.
 #'
 #' @details This function corresponds to \code{flip()} from NumPy.
+#' Flipping along an axis can be exemplified with a matrix. If the order of the elements along the first dimension (row) is to be reversed,
+#' it is helpful to imagine a horizontal axis (from left to right) in the middle of the matrix where flipping takes place. The first row
+#' becomes the last, the second row the second last and so on until the last row becomes the first. The same applies for reversing the order
+#' of the elements along the second dimension (column), with the distinction that the flipping axis is a vertical axis (from top to bottom)
+#' in the middle of the matrix.
+#'
 #' @return The reversed array \code{a} along axes.
 #'
 #' @export
